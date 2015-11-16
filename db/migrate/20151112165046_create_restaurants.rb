a0ab4317ea07127984b874f950f5177dc21c7fcc
SoyaTakahashi <appleeeen0525@gmail.com>
2015-11-17 02:19:55 +0900

    add restaurants table

--- db/migrate/20151112165046_create_restaurants.rb
+++ db/migrate/20151112165046_create_restaurants.rb
@@ -1,7 +1,12 @@
 class CreateRestaurants < ActiveRecord::Migration
   def change
     create_table :restaurants do |t|
-
+      t.string :name
+      t.string :name_kana
+      t.text :link
+      t.text :image_url
+      t.string :postal_code
+      t.text :addres
       t.timestamps null: false
     end
   end
