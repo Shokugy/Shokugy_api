79d1b5667e68dcec370982d65bf78455de16caed
SoyaTakahashi <appleeeen0525@gmail.com>
2015-12-20 21:02:35 +0900

    [fix]コード修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
 require "csv"
 
 CSV.foreach('db/restaurants.csv') do |row|
-  Subject.create(id: row[0], name: row[1], name_kana: row[2], link: row[3], image_url: row[4], postal_code: row[5], addres: row[6])
+  Restaurant.create(id: row[0], name: row[1], name_kana: row[2], link: row[3], image_url: row[4], postal_code: row[5], addres: row[6])
 end
