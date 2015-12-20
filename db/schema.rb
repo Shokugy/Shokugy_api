34cc63c045735a9ee6f8a2523d6df81f49a43d59
SoyaTakahashi <appleeeen0525@gmail.com>
2015-12-20 22:13:29 +0900

    [add]usersテーブルにidを追加

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151220122133) do
+ActiveRecord::Schema.define(version: 20151220131116) do
 
   create_table "group_users", force: :cascade do |t|
     t.integer  "group_id",   limit: 4
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 20151220122133) do
     t.string   "name",       limit: 255
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
+    t.string   "uid",        limit: 255
   end
 
   add_foreign_key "group_users", "groups"
