10e5d1ff47f3d9d9c1e77751aacc7dbf0a75a016
SoyaTakahashi <appleeeen0525@gmail.com>
2015-12-22 18:41:56 +0900

    [add]set_geocodeメソッドをRestaurant.rbに追加

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,4 +2,21 @@ class Restaurant < ActiveRecord::Base
   geocoded_by :address
   after_validation :geocode, if: Proc.new { |a| a.address_changed? }
 
+
+  class << self
+    def set_geocode(restaurants)
+      restaurants.each do |restaurant|
+        if restaurant.geocode
+          restaurant.update(latitude: restaurant.geocode[0], longitude: restaurant.geocode[1])
+        else
+          postal_code = restaurant.postal_code
+          latitude = GoogleGeocoding.instance.geocode_from(postal_code)[:latitude]
+          longitude = GoogleGeocoding.instance.geocode_from(postal_code)[:longitude]
+          restaurant.update(latitude: latitude, longitude: longitude)
+        end
+        sleep(1)
+      end
+    end
+  end
+
 end
