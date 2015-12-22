f9dc8d96247dbe5413de6b4b68f0dce4c7592023
SoyaTakahashi <appleeeen0525@gmail.com>
2015-12-22 18:18:20 +0900

    [add]reviews.rb作成

---
+++ app/apis/api/v1/reviews.rb
@@ -0,0 +1,74 @@
+module API
+  module V1
+    class Reviews < Grape::API
+      helpers do
+        # Strong Parametersの設定
+        def create_params
+          ActionController::Parameters.new(params).permit(:review, :rate, :restaurant_id, :user_id)
+        end
+
+        def set_review
+          @review = Review.find(params[:id])
+        end
+
+        # パラメータのチェック
+        # パラメーターの必須、任意を指定することができる。
+        # use :attributesという形で使うことができる。
+        params :create do
+          requires :review, type: String, desc: "Review review."
+          requires :rate, type: Float, desc: "Review rate."
+          requires :restaurant_id, type: Integer, desc: "Review restaurant_id"
+          requires :user_id, type: Integer, desc: "Review user_id"
+        end
+
+        # パラメータのチェック
+        params :id do
+          requires :id, type: Integer, desc: "Review id."
+        end
+      end
+
+      resource :reviews do
+        desc 'GET /api/v1/reviews'
+        get '/', jbuilder: 'api/v1/reviews/index' do
+          @reviews = Review.where(id: 1300..1303)
+        end
+
+        desc 'POST /api/v1/reviews/create'
+        params do
+          use :create
+        end
+        post '', jbuilder: 'api/v1/reviews/create' do
+          review = Review.new(create_params)
+          @error_message = review.error.full_messages unless review.save
+        end
+
+        desc 'GET /api/v1/message_boards/:id'
+        params do
+          use :id
+        end
+        get '/:id', jbuilder: 'api/v1/message_boards/show' do
+          set_message_board
+        end
+
+        desc 'PUT /api/v1/message_boards/:id'
+        params do
+          use :id
+          use :create
+        end
+        put '/:id' do
+          set_message_board
+          @message_board.update(message_board_params)
+        end
+
+        desc 'DELETE /api/v1/message_boards/:id'
+        params do
+          use :id
+        end
+        delete '/:id' do
+          set_message_board
+          @message_board.destroy
+        end
+      end
+    end
+  end
+end
