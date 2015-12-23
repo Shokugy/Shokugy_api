1ed20f54ba4eee264c2454cf256def3c8b180c6a
SoyaTakahashi <appleeeen0525@gmail.com>
2015-12-23 20:01:47 +0900

    [modify]緯度経度カラムをseeds.rbに追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
 require "csv"
 
 CSV.foreach('db/restaurants.csv') do |row|
-  Restaurant.create(id: row[0], name: row[1], name_kana: row[2], link: row[3], image_url: row[4], postal_code: row[5], address: row[6])
+  Restaurant.create(id: row[0], name: row[1], name_kana: row[2], link: row[3], image_url: row[4], postal_code: row[5], address: row[6], latitude: row[9], longitude: row[10])
 end
