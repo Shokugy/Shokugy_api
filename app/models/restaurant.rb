a10fd0ace454db275c8b595ce28e80d6b20ed093
SoyaTakahashi <appleeeen0525@gmail.com>
2015-12-24 04:21:35 +0900

    [modify]set_geocodeのsleeep時間を0.2秒に変更

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -18,7 +18,7 @@ class Restaurant < ActiveRecord::Base
           longitude = GoogleGeocoding.instance.geocode_from(postal_code)[:longitude]
           restaurant.update(latitude: latitude, longitude: longitude)
         end
-        sleep(1)
+        sleep(0.2)
       end
     end
   end
