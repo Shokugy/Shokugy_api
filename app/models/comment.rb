f12c1e39d8d3ce889b3f6a9735ead4fe578f9f0c
SoyaTakahashi <appleeeen0525@gmail.com>
2015-12-25 09:27:06 +0900

    [add]Comment validation

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,4 +3,8 @@ class Comment < ActiveRecord::Base
   belongs_to :invite
   belongs_to :user
 
+  # validation
+  validates_presence_of :text, on: :create
+  validation :text, length: { maximum: 140 }
+
 end
